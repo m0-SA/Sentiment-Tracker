975c0f6f233dcc2a9d7a3805d68790a0919f90c6
m0-SA <m0sahluwalia@gmail.com>
2026-08-20 01:50:04 +0100

    Insert Guardian Request Data

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import json
 
+from db import get_connection
 from sources.guardian import guardian_request
 
-i = guardian_request("Climate Change")
-with open("dataDump.json", "w") as file:
-    for x in i:
-        x["fetched"] = x["fetched"].isoformat()
+guardian_request_data = guardian_request("Climate Change")
+# with open("dataDump.json", "w") as file:
+#    for item in guardian_request_data:
+#        item["fetched"] = item["fetched"].isoformat()
 
-    json.dump(i, file, indent=4)
+#    json.dump(guardian_request_data, file, indent=4)
+
+with get_connection() as conn, conn.cursor() as cur:
+    cur.executemany(
+        "INSERT INTO mentionData (UniqueID, Title, Source, SourceType, Topic, Content, publishDate, fetched)"
+        "VALUES (%(uniqueID)s, %(Title)s, %(Source)s, %(SourceType)s, %(Topic)s, %(Content)s, %(publishDate)s, %(fetched)s)"
+        "ON CONFLICT (UniqueID) DO NOTHING",
+        guardian_request_data,
+    )
+    print("Guardian Items Added...")
+    conn.commit()
