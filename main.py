d0899e4c7410b8ec4699bea709585b055ea90317
m0-SA <m0sahluwalia@gmail.com>
2026-08-24 00:55:35 +0100

    Aggregate Score

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 from insert import insert_articles
-from scoring import append_scores
+from scoring import aggregate_score, aggregate_score_all, append_scores
 from sources.gnews import gnews_request
 from sources.guardian import guardian_request
 from sources.rss import rss_request
@@ -11,12 +11,26 @@ topic = "Climate Change"
 request_functions = [guardian_request, gnews_request, rss_request]
 
 sid = SentimentIntensityAnalyzer()
+sources = []
 
 for func in request_functions:
     try:
         request_data = func(topic)
+
         for dictionary in request_data:
+            source_type = dictionary.get("SourceType")
+            if "rss" in source_type.lower() and "rss" not in sources:
+                sources.append("rss")
+            elif source_type not in sources:
+                sources.append(dictionary.get("SourceType"))
+
             append_scores(dictionary, sid)
+
         insert_articles(request_data)
     except Exception as e:  # noqa: BLE001
         print(f"Failed to execute {func.__name__}: {e}")
+
+total_aggregate_score = aggregate_score_all(topic)
+sources_aggregate_score = {}
+for source in sources:
+    sources_aggregate_score.update({source: aggregate_score(topic, source)})
--- scoring.py
+++ scoring.py
@@ -1,3 +1,6 @@
+from db import get_connection
+
+
 def append_scores(data_dict, sid):
     if data_dict.get("Content") is None:
         data_dict.update(
@@ -24,3 +27,44 @@ def append_scores(data_dict, sid):
     )
 
     return data_dict
+
+
+def aggregate_score_all(topic):
+
+    data = {"topic": topic}
+
+    with get_connection() as conn, conn.cursor() as cur:
+        cur.execute(
+            "SELECT SUM(CASE WHEN SourceType = 'Guardian' THEN compound*2 ELSE compound*1 END)/ SUM(CASE WHEN SourceType = 'Guardian' THEN 2 ELSE 1 END) "
+            "FROM mentionData "
+            "WHERE Topic = %(topic)s AND Compound IS NOT NULL ",
+            data,
+        )
+        all_sources = cur.fetchone()
+
+        return all_sources[0]
+
+
+def aggregate_score(topic, source_type):
+
+    data = {"topic": topic, "source_type": source_type}
+
+    with get_connection() as conn, conn.cursor() as cur:
+        if "RSS".lower() in source_type.lower():
+            cur.execute(
+                "SELECT AVG(Compound) "
+                "FROM mentionData "
+                "WHERE Topic = %(topic)s  AND SourceType LIKE '%%RSS%%' AND Compound IS NOT NULL ",
+                data,
+            )
+        else:
+            cur.execute(
+                "SELECT AVG(Compound) "
+                "FROM mentionData "
+                "WHERE Topic = %(topic)s AND SourceType = %(source_type)s AND Compound IS NOT NULL ",
+                data,
+            )
+
+        all_sources = cur.fetchone()
+
+        return all_sources[0]
